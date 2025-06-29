ce884c20511f56181169ff4049e5712092ae8ae3
vkghb <monetizaplsoia@gmail.com>
2025-06-29 12:08:39 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
-echo "Iniciando servidor..."
+# Inicia o servidor web falso EM SEGUNDO PLANO
+echo 
 python3 /app.py &
 
-
+# Dá um tempo para o servidor web iniciar
 sleep 5
 
-
-if [ -z "$VIDEO_URL" ] || [ -z "$RTMP_URL" ] || [ -z "$STREAM_KEY" ]; then
-  echo "Erro: As variáveis de ambiente VIDEO_URL, RTMP_URL e STREAM_KEY devem ser definidas."
+# Verifica se as variáveis de ambiente essenciais foram definidas
+# Note que agora temos IMAGE_URL e AUDIO_URL em vez de VIDEO_URL
+if [ -z "$IMAGE_URL" ] || [ -z "$AUDIO_URL" ] || [ -z "$RTMP_URL" ] || [ -z "$STREAM_KEY" ]; then
+  echo "Erro: As variáveis IMAGE_URL, AUDIO_URL, RTMP_URL e STREAM_KEY devem ser definidas."
   exit 1
 fi
 
-echo "Iniciando FFmpeg..."
+echo 
 
-
-ffmpeg -re -stream_loop -1 \
--i "$VIDEO_URL" \
--c:v copy \
--c:a aac -ar 44100 -b:a 128k \
--f flv "$RTMP_URL/$STREAM_KEY"
+# --- O NOVO COMANDO MÁGICO DO FFMPEG ---
+ffmpeg \
+    -loop 1 -r 2 -i "$IMAGE_URL" \
+    -stream_loop -1 -re -i "$AUDIO_URL" \
+    -c:v libx264 -preset veryfast -tune stillimage -pix_fmt yuv420p \
+    -c:a aac -b:a 128k -ar 44100 \
+    -f flv "$RTMP_URL/$STREAM_KEY"
